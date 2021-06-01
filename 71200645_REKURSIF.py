1807e83cc28a79f1b0b70a348877b133e52d9855
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2021-06-01 20:06:26 +0700

    Update 71200645_REKURSIF.py

--- 71200645_REKURSIF.py
+++ 71200645_REKURSIF.py
@@ -2,13 +2,13 @@
 # 71200645 - Edith Felicia Putri
 
 '''
-Indiana ingin membuat daftar impian yang ingin dikunjunginya
-sambil menunggu hari ulang tahunnya pada waktu tengah malam.
-Buatlah dengan fungsi rekursif perhitungan mundur dan
-harapan-harapan Indiana.
+Indiana ingin membuat daftar impian yang ingin diinginkannya
+sebanyak waktu hitung mundur sampai hari ulang tahunnya pada 
+waktu tengah malam. Buatlah dengan fungsi rekursif 
+perhitungan mundur dan harapan-harapan Indiana.
 '''
 
-def tanya_mundur(num):
+def hitung_mundur(num):
     simpan = []
     if (num == 0):
         print(" ")
@@ -18,7 +18,7 @@ def tanya_mundur(num):
         print(num)
         tanya = input("Impian: ")
         next_num = num - 1
-        tanya_mundur(next_num)
+        hitung_mundur(next_num)
         simpan.append(tanya)
         print("SEMOGA IMPIANMU TERWUJUD!")
         print(simpan)
@@ -27,4 +27,4 @@ def tanya_mundur(num):
 nama = input("Masukkan nama: ")
 umur = int(input("Masukkan umur: "))
 angka = int(input("Masukkan waktu: "))
-print(tanya_mundur(angka))
+print(hitung_mundur(angka))
